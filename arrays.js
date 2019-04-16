b8bbe229fa034f87104f991ee81ea488d269760f
Ititi Obidah <iobidah9@gmail.com>
2019-04-16 03:07:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,5 +11,7 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
    array.unshift(element);
-   return array
+   return array;
 }
+
+function 

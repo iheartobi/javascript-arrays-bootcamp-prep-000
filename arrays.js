1915630b023601c26089578512c782afe98d1477
Ititi Obidah <iobidah9@gmail.com>
2019-04-16 03:12:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,8 +14,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
    return array;
 }
 
-function addElementToEndOfArray(array, element){
- return [array..., element];
+function addElementToEndOfArray(array, element) {
+ return [...array, element];
 } 
 
 function destructivelyAddElementToEndOfArray(array, element) {
